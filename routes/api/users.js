e72f957614c14574ec304afdc2f2ca3ad97add2a
Frederic Chhean <chheanfrederic@gmail.com>
2019-05-06 12:01:33 +0200

    initial express servir with routes

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router(); 
+
+
+//@route GET api/users/test
+//@des Tests post route
+//@access Public 
+router.get("/test", (req,res) =>
+    res.json({msg: "Users works"})
+); 
+
+module.exports = router;
