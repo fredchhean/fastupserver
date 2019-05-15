1b894dff96c733f35f829e0741e803344a78b1d2
Frederic Chhean <chheanfrederic@gmail.com>
2019-05-15 02:18:26 +0200

    deletebutton for users and profiles works

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from "./types";
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from "./types";
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -30,6 +30,25 @@ export const createProfile = (profileData, history) => dispatch => {
       }))
 }
 
+
+//delete account & profile
+export const deleteAccount = () => dispatch => {
+  if(window.confirm("Are you sure that you want to delete your account?")){
+    axios
+      .delete("http://localhost:8000/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+        ).catch(err => 
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+          })
+          )
+  }
+}
 
 //profile loading 
 export const setProfileLoading = () => {
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {connect} from "react-redux";
-import {getCurrentProfile} from "../../actions/profileActions";
+import {getCurrentProfile, deleteAccount} from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import {Link} from "react-router-dom";
+import ProfileActions from "./ProfileActions";
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
   }
+
+  onDeleteClick(e){
+    this.props.deleteAccount();
+
+  }
+
   render() {
     const {user} = this.props.auth;
     const { profile,loading } = this.props.profile;
@@ -20,7 +27,13 @@ class Dashboard extends Component {
     } else {
       //check if logged in user has a profile
       if(Object.keys(profile).length > 0){
-        dashboardContent = <h4>TODO : DISPLAY PROFILE</h4>
+        dashboardContent = (
+          <div>
+          <p>Welcome <Link to={`/profile/${profile.handle}`}>{user.firstname}</Link></p>
+          <ProfileActions/>
+          <button onClick={this.onDeleteClick.bind(this)}>Delete my account</button>
+          </div>
+        );
       } else {
         //user is logged in but has no profile
         dashboardContent = (
@@ -48,6 +61,7 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth:PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 }
@@ -57,4 +71,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
+export default connect(mapStateToProps, {getCurrentProfile, deleteAccount})(Dashboard);
